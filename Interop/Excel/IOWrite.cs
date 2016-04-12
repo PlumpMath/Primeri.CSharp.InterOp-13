1a5a7e055f654702b8e8f36adddbd85dc4483c0f
Ivan Vasilev <i.vasilev@storact.internal>
2016-04-12 12:24:42 +0300

    Modul09 - Video 07 - Създаване и запаметяване на фаил

--- Interop/Excel/IOWrite.cs
+++ Interop/Excel/IOWrite.cs
@@ -6,7 +6,7 @@ namespace Excel
 	public class IOWrite
 	{
 		private DataStruct _data;
-		private InteropExcel.Application Excel;
+		private InteropExcel.Application excel;
 
 		public IOWrite (DataStruct data)
 		{
@@ -16,7 +16,25 @@ namespace Excel
 		{
 			try
 			{
-				//Междинни проверки
+				//Подготовка
+				excel = InteropExcel.Application ();
+
+				if (excel == null) return false;
+
+				InteropExcel.Workbook workbook = excel.Workbooks.Add ();
+				if (workbook == null) return false;
+
+
+				//Попълване на таблицата
+
+
+
+				//Запаметяване
+				workbook.SaveCopyAs ( getPath() );
+
+				excel.DisplayAlerts = false; //Изключваме съобщенията на Ексел
+				workbook.Close ();
+				Excel.Quit ();
 
 				return true;				
 			}catch{
